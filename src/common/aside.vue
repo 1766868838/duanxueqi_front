308717bcf472ea8607d612ce6b4db80dfd6e3da9
laogaoll <104480885+laogaoll@users.noreply.github.com>
2023-06-26 11:23:02 +0800

    Update aside.vue
    更新路由

--- src/common/aside.vue
+++ src/common/aside.vue
@@ -17,7 +17,9 @@
         <el-menu-item index="1-1">
           <router-link to="/Badmin/bmanage"> 借阅管理</router-link>
         </el-menu-item>
-        <el-menu-item index="1-2">图书管理</el-menu-item>
+        <el-menu-item index="1-2">
+           <router-link to="/Badmin/librarian"> 图书管理</router-link>
+        </el-menu-item>
         <el-menu-item index="1-3">图书查询</el-menu-item>
         <el-menu-item index="1-4">读者管理</el-menu-item>
         <el-menu-item index="1-5">个人主页</el-menu-item>
